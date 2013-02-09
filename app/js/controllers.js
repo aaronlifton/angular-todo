363a5c2521f48afc9cbc7d7d5b182cdc5eb3b012
Daniel Berkompas <dberkom@gmail.com>
2013-02-09 10:12:25 -0500

    Helpful comments

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,38 +24,46 @@ function TodoCtrl($scope) {
     {id: 1, text:'learn angular', done: true},
     {id: 2, text:'build an angular app', done: false}];
 
+  // Persist any changes to $scope.todos to local storage
   $scope.$watch('todosJson()', function(result) {
     window.localStorage.todos = result;
   });
   
+  // Play victory sound whenever there are no todos left
   $scope.$watch('todos.length', function(length) {
     if (length === 0 && $scope.finishedTodo === true) {
       $scope.playVictorySound();
     }
   });
 
+  // Convert todos to JSON for easy monitoring
   $scope.todosJson = function() {
     return angular.toJson($scope.todos)
   };
 
+  // Add a todo to $scope.todos
   $scope.addTodo = function() {
     $scope.todos.push({id: randomString(5), text:$scope.todoText, done:false});
     $scope.todoText = '';
   };
 
+  // Remove a todo from $scope.todos
   $scope.deleteTodo = function(todo) {
     $scope.todos.push.apply($scope.todos.splice($scope.todos.indexOf(todo), $scope.todos.length));
     $scope.finishedTodo = true;
   }
 
+  // Put a todo in edit mode
   $scope.startEditing = function(todo) {
     todo.isEditing = true;
   };
 
+  // Take a todo out of edit mode
   $scope.stopEditing = function(todo) {
     todo.isEditing = false;
   }
 
+  // Count the number of remaining todos
   $scope.remaining = function() {
     var count = 0;
     angular.forEach($scope.todos, function(todo) {
@@ -64,10 +72,12 @@ function TodoCtrl($scope) {
     return count;
   };
 
+  // Count the number of completed todos
   $scope.completed = function() {
     return $scope.todos.length - $scope.remaining();
   };
  
+  // Delete all completed todos
   $scope.archive = function() {
     var oldTodos = $scope.todos;
     $scope.todos = [];
@@ -77,6 +87,7 @@ function TodoCtrl($scope) {
     $scope.finishedTodo = true;
   };
 
+  // Play a victory jingle
   $scope.playVictorySound = function() {
     var sound = document.getElementById("victorySound");
     if (window.chrome) sound.load();
