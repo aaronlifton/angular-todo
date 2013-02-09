cf9defa0b0392e892987f2226832c6a25abb369d
Daniel Berkompas <daniel@managemyproperty.com>
2013-02-08 20:25:45 -0800

    Functional task manager

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -19,4 +19,38 @@ function TodoCtrl($scope) {
   $scope.stopEditing = function(todo) {
     todo.isEditing = false;
   }
+
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  $scope.completed = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 1 : 0;
+    });
+    return count
+  };
+ 
+  $scope.archive = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+
+    if ($scope.todos.length == 0) {
+      $scope.playVictorySound()
+    }
+  };
+
+  $scope.playVictorySound = function() {
+    var sound = document.getElementById("victorySound");
+    if (window.chrome) sound.load();
+    sound.play()
+  };
 }
