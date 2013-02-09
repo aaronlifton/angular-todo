210c6b70cbc27defe09bb26ab9442e7fe2788a1e
Daniel Berkompas <daniel@managemyproperty.com>
2013-02-08 18:38:09 -0800

    Add bootstrap.css, functional todos

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,14 +4,19 @@
 
 function TodoCtrl($scope) {
   $scope.todos = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false}];
+    {id: 1, text:'learn angular', done: true},
+    {id: 2, text:'build an angular app', done: false}];
+
+  $scope.addTodo = function() {
+    $scope.todos.push({id: Math.floor(Math.random()*1000), text:$scope.todoText, done:false});
+    $scope.todoText = '';
+  };
+
+  $scope.startEditing = function(todo) {
+    todo.isEditing = true;
+  };
+
+  $scope.stopEditing = function(todo) {
+    todo.isEditing = false;
+  }
 }
-
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
